89aa8a4028a0a43347c2a6463d6925b2baef777e
nicolireislima <nicolireis.veleiros@gmail.com>
2025-05-14 16:12:17 -0300

    update produtos

--- js/script.js
+++ js/script.js
@@ -75,6 +75,21 @@
             }
         }) 
     }
+
+
+    //Isotope - Biblioteca para Filtro
+    window.addEventListener('load', () => {
+        let portfolioContainer = select(".portfolio-container")
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemsSelector: '.portfolio-item'
+            });
+
+            let portfolioFilters = select('#portfolio-filters', true);
+        }
+         
+
+    });
         
 
 
